9ec78d28f1eca2e2e064f43d73c6b4093eb02ba3
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-01-19 16:33:48 +0100

    added api from laravel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <section class="container pt-5">
-    <h1>Titolo <i class="fa-solid fa-house"></i></h1>
+    <h1>Portfolio <i class="fa-solid fa-house"></i></h1>
     <CarouselComponent />
+    <!--   
+      <ul>
+    <li v-for="project in store.projects" :key="project.id">
+        {{ project.title }}
+      </li>
+    </ul>
+     <div class="d-flex">
+      <button @click="previousPage">Indietro</button>
+      <button @click="nextPage">Avanti</button>
+    </div> 
+    -->
   </section>
 </template>
 
@@ -16,12 +27,39 @@ export default {
   data() {
     return {
       store,
+      currentPage: 1,
+      lastPage: 0,
     }
   },
   methods: {
     getAllProjects() {
+      axios.get(store.apiUrl + '/projects', { params: { page: this.currentPage } }).then((res) => {
+        console.log(res.data);
+        this.store.projects = res.data.results.data;
+        this.store.projectsLinks = res.data.results;
+        console.log(`links`, this.store.projectsLinks);
+        console.log(this.store.projects);
+        this.currentPage = res.data.results.current_page;
+        this.lastPage = res.data.results.last_page;
+      });
+    },
+    nextPage() {
+      if (this.currentPage < this.lastPage) {
+        this.currentPage = this.currentPage + 1;
+      } else {
+        this.currentPage = 1;
+      }
+      this.getAllProjects();
+    },
+    previousPage() {
+      if (this.currentPage === 1) {
+        this.currentPage = this.lastPage;
+      } else {
+        this.currentPage = this.currentPage - 1;
+      }
+      this.getAllProjects();
+    },
 
-    }
   },
   mounted() {
     this.getAllProjects()
--- src/components/partials/CarouselComponent.vue
+++ src/components/partials/CarouselComponent.vue
@@ -1,13 +1,16 @@
 <template>
     <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
-        <Slide v-for="slide in 10" :key="slide" class="border">
-            <div class="carousel__item">sopra {{ slide }}</div>
+        <Slide v-for="project in store.projects" :key="project.id" class="border">
+            <div class="carousel__item">
+                <h2>{{ project.title }}</h2>
+                <h6>{{ project.category.name }}</h6>
+            </div>
         </Slide>
     </Carousel>
 
     <Carousel id="thumbnails" :items-to-show="4" :wrap-around="true" v-model="currentSlide" ref="carousel">
-        <Slide v-for="slide in 10" :key="slide">
-            <div class="carousel__item border" @click="slideTo(slide - 1)">sotto {{ slide }}.</div>
+        <Slide v-for="project in store.projects" :key="project.id">
+            <div class="carousel__item border" @click="slideTo(slide - 1)">{{ project.technologies[0].name }}</div>
         </Slide>
     </Carousel>
 </template>
@@ -17,12 +20,13 @@
 import { defineComponent } from 'vue';
 import { Carousel, Slide } from 'vue3-carousel';
 import 'vue3-carousel/dist/carousel.css';
-
+import { store } from './../../data/store.js';
 export default {
     components: { Carousel, Slide, Navigation, },
     name: 'CarouselComponent',
     data() {
         return {
+            store,
             currentSlide: 0,
         }
     },
@@ -45,7 +49,7 @@ export default {
     .carousel__item {
         min-height: 100px;
         cursor: pointer;
-        min-width: 300px;
+        min-width: 200px;
     }
 }
 </style>
