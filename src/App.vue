164d3df0dcf593de230c94cadaf8eb409e6901ef
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-01-19 15:32:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container pt-5">
+    <h1>Titolo <i class="fa-solid fa-house"></i></h1>
+    <CarouselComponent />
+  </section>
+</template>
+
+<script>
+import { store } from './data/store.js';
+import axios from 'axios';
+
+import CarouselComponent from './components/partials/CarouselComponent.vue';
+export default {
+  components: { CarouselComponent },
+  name: 'App',
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getAllProjects() {
+
+    }
+  },
+  mounted() {
+    this.getAllProjects()
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '/src/assets/style/partials/_variables.scss' as *;
+
+h1 {
+  color: $primary-color !important;
+}
+</style>
---
+++ src/components/partials/CarouselComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
+        <Slide v-for="slide in 10" :key="slide" class="border">
+            <div class="carousel__item">sopra {{ slide }}</div>
+        </Slide>
+    </Carousel>
+
+    <Carousel id="thumbnails" :items-to-show="4" :wrap-around="true" v-model="currentSlide" ref="carousel">
+        <Slide v-for="slide in 10" :key="slide">
+            <div class="carousel__item border" @click="slideTo(slide - 1)">sotto {{ slide }}.</div>
+        </Slide>
+    </Carousel>
+</template>
+  
+
+<script>
+import { defineComponent } from 'vue';
+import { Carousel, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+export default {
+    components: { Carousel, Slide, Navigation, },
+    name: 'CarouselComponent',
+    data() {
+        return {
+            currentSlide: 0,
+        }
+    },
+    methods: {
+        slideTo(val) {
+            this.currentSlide = val
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+#gallery {
+    .carousel__item {
+        height: 300px;
+    }
+}
+
+#thumbnails {
+    .carousel__item {
+        min-height: 100px;
+        cursor: pointer;
+        min-width: 300px;
+    }
+}
+</style>
